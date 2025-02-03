5684c44073a9d27b63c2555911fc80281e954293
Sofilayerdi <sofilayerdi@gmail.com>
2025-02-03 12:17:02 -0600

    se agrego la clase de stackWithVector

---
+++ demo/src/main/java/ht2/StackWithVector.java
@@ -0,0 +1,29 @@
+public class StackWithVector<T> implements IStack<T>{
+    IStack<T> miVector = new Vector<>();
+    
+    @Override
+    public void push(T item){
+        miVector.add(element);
+    }
+
+    @Override
+    public T pop(){
+        return miVector.remove(stack.size() - 1);
+    }
+
+    @Override
+    public T peek(){
+        return miVector.get(stack.size() -1)
+    }
+
+    @Override
+    public boolean isEmpty(){
+        if (miVector.size() == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
